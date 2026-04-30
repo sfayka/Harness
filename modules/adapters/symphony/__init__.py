79dde4fd8e46885f1e40332bd9ffeafe343b59b6
Sean Fay <sean.fay@knoxanalytics.com>
2026-04-30 09:46:28 -0400

    Add Symphony intent handoff adapter

---
+++ modules/adapters/symphony/__init__.py
@@ -0,0 +1,11 @@
+"""Symphony-compatible execution substrate adapter."""
+
+from modules.adapters.symphony.execution_substrate_adapter import (
+    SymphonyExecutionSubstrateAdapter,
+    SymphonyHandoffPayload,
+)
+
+__all__ = [
+    "SymphonyExecutionSubstrateAdapter",
+    "SymphonyHandoffPayload",
+]
